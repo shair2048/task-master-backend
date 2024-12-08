4a17a5f75693f30212f50af5e0a9921165b0b234
Ken <sonhai.it.2048@gmail.com>
2024-12-08 15:01:58 +0700

    add username for account model

--- models/account.model.js
+++ models/account.model.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const AccountSchema = mongoose.Schema(
   {
+    username: {
+      type: String,
+      required: [true, "Plase enter username"],
+    },
     email: {
       type: String,
       required: [true, "Plase enter email"],
